30ee78609fbe1dd93b41879014b77ccc951f3579
Vaibhav <vaibhav.maurya@centricconsulting.com>
2021-07-12 14:10:16 +0530

    Video 78 :   Creating roles in asp net core

---
+++ EmployeeManagementUsingIdentity/Controllers/AdministrationController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagementUsingIdentity.ViewModelsIdentity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementUsingIdentity.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> rydorolemanager;
+
+        public AdministrationController(RoleManager<IdentityRole> rydorolemanager)
+        {
+            this.rydorolemanager = rydorolemanager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel rydomodel)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name= rydomodel.RoleName
+                };
+
+                IdentityResult result =  await rydorolemanager.CreateAsync(identityRole);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+            }
+            return View();
+        }
+    }
+}
